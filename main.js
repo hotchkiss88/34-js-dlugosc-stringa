4a3dd2803afe4fa719ae14db4385ffd12d7382b4
hotchkiss88 <al-sowa@hotmail.com>
2021-08-31 16:28:39 +0200

    34-js-dlugosc-stringa

---
+++ main.js
@@ -0,0 +1,13 @@
+let first = "Uwielbiam JavaScript";
+
+let second = "Jestem świetnym programistą";
+
+function smush(x, y) {
+
+    let result = x + y;
+    return result
+};
+
+
+let effect = smush(first, second);
+console.log(effect);
